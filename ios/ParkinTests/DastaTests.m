d989bfaf3320e87224bf44c00d545f1a4f7e48cb
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2024-04-01 08:56:34 +0500

    first

--- ios/ParkinTests/DastaTests.m
+++ ios/ParkinTests/DastaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DastaImamiaTests : XCTestCase
+@interface ParkinTests : XCTestCase
 
 @end
 
-@implementation DastaImamiaTests
+@implementation ParkinTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
